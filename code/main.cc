b3bc79b9f0b21d025500cdd28ccba00b8d941ad9
chenyao <chenyao@dev6.(none)>
2019-04-24 19:54:57 +0800

    init

---
+++ code/main.cc
@@ -0,0 +1,22 @@
+#include "stdio.h"
+
+bool g_game_loop = true;
+
+int main(int argc, char** argv)
+{
+	// init
+
+	// run
+	while(g_game_loop)
+	{
+		// handle msg
+
+		// handle time event
+
+		// frame update
+	}
+
+	// uninit
+
+	return 0;
+}
